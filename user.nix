f1335291a7b10470bf4348fd1f4490314d08e638
Jair Sánchez <78786202+jx11r@users.noreply.github.com>
2022-02-06 23:42:37 -0600

    Remove user packages

--- user.nix
+++ user.nix
@@ -12,41 +12,7 @@
       extraGroups = [ "wheel" ];
 
       packages = with pkgs; [
-        # Hardware
-        pulseaudio-ctl
-        brightnessctl
-
-        # Shell Utilities
-        gitstatus
-        zsh-autosuggestions
-        zsh-syntax-highlighting
-
-        # Utilities
-        sxiv
-        rofi
-        papirus-icon-theme
-        neofetch
-        htop
-
-        # Code Editor
-        vscodium
-        emacs
-        neovim
-
-        # File Manager
-        xfce.thunar
-        xfce.thunar-volman
-
-        # Social Media
-        # whatsapp-for-linux
-        tdesktop
-
-        # Desktop
-        # gpicview
-        # gimp
-        # spotify
-        lxappearance
-        gnome.gnome-screenshot
+        # ...
       ];
     };
   };
