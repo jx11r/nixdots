42ba3b7266fba8f0fd9a403808878d5cb4085d02
Jair Sánchez <78786202+jx11r@users.noreply.github.com>
2022-02-06 23:41:40 -0600

    Add new packages

--- systemPackages.nix
+++ systemPackages.nix
@@ -3,31 +3,57 @@
 {
   environment.systemPackages = with pkgs; [
     # Hardware
+    pulseaudio-ctl
+    brightnessctl
     ntp
 
     # Terminal
+    # xterm
     alacritty
+    
+    # Shell Utilities
+    # gitstatus
+    # zsh-autosuggestions
+    # zsh-syntax-highlighting
 
     # Utilities
     unzip
-    gcc
     wget
-    curl
     git
+    sxiv
+    rofi
+    papirus-icon-theme
+    neofetch
+    htop
     exa
     bat
 
     # Code Editor
+    # vscodium
+    # emacs
+    neovim
     nano
     vim
 
     # File Manager
+    xfce.thunar
+    xfce.thunar-volman
     ranger
 
     # Browser
     # w3m
     # ungoogled-chromium
     firefox
+
+    # Social Media
+    # whatsapp-for-linux
+    tdesktop
+
+    # Desktop
+    # gpicview
+    # gimp
+    # spotify
+    lxappearance
   ];
 
   fonts.fonts = with pkgs; [
