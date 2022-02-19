ec1217b32bf481c2d5ed8b913c0c78a978317d7d
jx11r <jx11r@hotmail.com>
2022-02-18 20:39:29 -0600

    Minor edit

--- system/gpu/amd.nix
+++ system/gpu/amd.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Xserver configuration
+  # Xserver Configuration
   services.xserver = {
     videoDrivers = [ "amdgpu" ];
 
@@ -34,5 +34,5 @@
 
   hardware.opengl.driSupport = true;
   # For 32 bit applications
-  #hardware.opengl.driSupport32Bit = true;
+  # hardware.opengl.driSupport32Bit = true;
 }
