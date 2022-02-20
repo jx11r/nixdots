b19a6950e74d245a3d3f4afed3bab6e49f46d4d3
jx11r <jx11r@hotmail.com>
2022-02-20 12:21:45 -0600

    Add kernel params

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -6,13 +6,22 @@
     cleanTmpDir = true;
     kernelPackages = pkgs.linuxPackages_latest;
 
+    kernelParams = [
+      "loglevel=3"
+      # "quiet"
+      "acpi_backlight=vendor"
+    ];
+
     loader = {
+      timeout = 15;
+
       grub = {
         enable = true;
         version = 2;
         devices = [ "nodev" ];
         efiSupport = true;
         useOSProber = false;
+        gfxmodeEfi = "1360x768";
       };
 
       efi = {
