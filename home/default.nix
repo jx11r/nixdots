5efe2d6625b9c16cfd7a978b410e0f41c03a2d95
jx11r <jx11r@hotmail.com>
2022-02-20 19:13:24 -0600

    Add home-manager

--- home/default.nix
+++ home/default.nix
@@ -1,5 +1,23 @@
+{ config, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchTarball
+    "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
+
 {
   imports = [
+    (import "${home-manager}/nixos")
     ./user.nix
   ];
+
+  home-manager.users.jx11r = {
+
+    programs = {
+      alacritty = {
+        enable = true;
+        settings = import ./config/alacritty.nix { };
+      };
+    };
+  };
 }
