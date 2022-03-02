f8fa6af9c76da35c2c579b8dc1330d4a8f097e15
jx11r <jx11r@hotmail.com>
2022-03-02 03:29:02 -0600

    Update config: starship.nix

--- home/config/starship.nix
+++ home/config/starship.nix
@@ -2,26 +2,32 @@
 
 {
   add_newline = false;
-  scan_timeout = 10;
+  cmd_duration.min_time = 15000;
   line_break.disabled = false;
+  package.disabled = true;
+  scan_timeout = 10;
 
   character = {
     success_symbol = "[λ](bold yellow)";
     error_symbol = "[λ](bold red)";
   };
 
-  cmd_duration = {
-    disabled = false;
-    min_time = 15000;
-  };
-
   directory = {
-    disabled = false;
     truncation_length = 2;
     read_only = "";
   };
 
-  package = {
-    disabled = true;
+  fill.symbol = " ";
+
+  format = ''
+    $directory$git_branch$git_status$cmd_duration$fill$all$character
+  '';
+
+  git_branch.format = "[$symbol$branch]($style) ";
+
+  git_status = {
+    ahead   = "";
+    behind  = "";
+    deleted = "-";
   };
 }
