27f3dee4033cc47595534c1e6103207601f10b01
jx11r <jx11r@hotmail.com>
2022-03-07 01:30:03 -0600

    Add install script

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Shell Colors
+reset="\033[0m"
+black="\033[1;30m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+blue="\033[1;34m"
+magenta="\033[1;35m"
+cyan="\033[1;36m"
+white="\033[1;37m"
+
+cl=${reset}
+cl0=${black}
+cl1=${red}
+cl2=${green}
+cl3=${yellow}
+cl4=${blue}
+cl5=${magenta}
+cl6=${cyan}
+cl7=${white}
+
+# Base System
+rm system/hardware-configuration.nix
+
+nixos-generate-config --root /mnt
+mv hardware-configuration.nix system/
+
+nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
+nix-channel --update
+
+nixos-install
+
+# Post Install
+pushd /mnt/home/*
+
+git clone https://github.com/jx11r/qtile.git .config/qtile
+
+printf "\n"
+echo -e "${cl1}Don't forget to run ${cl2}nixos ${cl5}--post-install ${cl1}after reboot to finish the system setup.${cl}"
+echo -e "${cl3}The initial user password is: ${cl7}1234${cl}"
