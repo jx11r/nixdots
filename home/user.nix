dc19846175b0d76c6dec6328091d0a1d62f52836
Jair Sánchez <78786202+jx11r@users.noreply.github.com>
2022-03-13 00:27:15 -0600

    Add user packages

--- home/user.nix
+++ home/user.nix
@@ -18,7 +18,59 @@
       password = "nix";
 
       packages = with pkgs; [
-        # ...
+        # [ Terminal ]
+        alacritty
+        unstable.wezterm
+        
+        # [ Code Editor ]
+        # vscodium
+        # emacs
+        unstable.neovim
+        
+        # [ Programs ]
+        starship
+        exa
+        bat
+        htop
+        # gtop
+        # gotop
+        # scrot (deprecated)
+        # peaclock
+        # feh
+        sxiv
+        # dmenu
+        rofi
+        neofetch
+        # cava
+        maim
+        
+        # [ File Manager ]
+        xfce.thunar
+        xfce.thunar-volman
+        ranger
+        
+        $ [ Browser ]
+        # ungoogled-chromium
+        # qutebrowser
+        # opera
+        # w3m
+        firefox
+        
+        # [ Chat ]
+        # whatsapp-for-linux
+        unstable.tdesktop
+        
+        # [ Desktop ]
+        papirus-icon-theme
+        # gnome.gnome-screenshot
+        # gpicview
+        # vlc
+        # evince
+        # libreoffice
+        # gimp
+        # spotify
+        # spotify-tui
+        lxappearance
       ];
     };
   };
